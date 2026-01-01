b4c11bc3478f38e8855e7cabae9b5a7c81917bdc
Rakib Hossain <hossainrokib55@gmail.com>
2026-01-01 18:38:58 +0600

    array problem

--- Search_an_element_in_an_array.cpp
+++ Search_an_element_in_an_array.cpp
@@ -1,8 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
- int main()
+
+int main()
 {
-    
-   
- return 0;
-} 
+    int n, x;
+    cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int flg = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == x)
+        {
+            flg = 1;
+        }
+    }
+    if (flg == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
